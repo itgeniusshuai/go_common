fbaadb1cf924064f07e67c87f0047d30ee449fbc
itgeniusshuai <1115870494@qq.com>
2018-04-29 15:40:38 +0800

    加密类

---
+++ common/DigestUtils.go
@@ -0,0 +1,159 @@
+package common
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+	"crypto/des"
+	"crypto/cipher"
+	"bytes"
+	"encoding/pem"
+	"errors"
+	"crypto/x509"
+	"crypto/rsa"
+	"crypto/rand"
+)
+
+const base64Table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+var coder = base64.NewEncoding(base64Table)
+
+func Md5(str string) string{
+	h := md5.New()
+	h.Write(StrToBytes(str))
+	x := h.Sum(nil)
+	y := make([]byte, 32)
+	hex.Encode(y, x)
+	return string(y)
+}
+
+func Base64EncodeStr(str string) string{
+	return coder.EncodeToString(StrToBytes(str))
+}
+
+func Base64EncodeBytes(str string) []byte{
+	var bs []byte
+	coder.Encode(bs, StrToBytes(str))
+	return bs
+
+}
+
+func Base64DecodeStr(str string) string{
+	return coder.EncodeToString(StrToBytes(str))
+}
+
+func Base64DecodeBytes(str string) []byte{
+	bs,err := coder.DecodeString(str)
+	if err != nil{
+		return nil
+	}
+	return bs
+}
+
+// Des加密
+func DesEncrypt(origData, key []byte) ([]byte, error) {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	origData = PKCS5Padding(origData, block.BlockSize())
+	// origData = ZeroPadding(origData, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key)
+	crypted := make([]byte, len(origData))
+	// 根据CryptBlocks方法的说明，如下方式初始化crypted也可以
+	// crypted := origData
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+// Des解密
+func DesDecrypt(crypted, key []byte) ([]byte, error) {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockMode := cipher.NewCBCDecrypter(block, key)
+	//origData := make([]byte, len(crypted))
+	origData := crypted
+	blockMode.CryptBlocks(origData, crypted)
+	//origData = PKCS5UnPadding(origData)
+
+	origData = ZeroUnPadding(origData)
+	return origData, nil
+}
+
+// 3DES加密
+func TripleDesEncrypt(origData, key []byte) ([]byte, error) {
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	origData = PKCS5Padding(origData, block.BlockSize())
+	// origData = ZeroPadding(origData, block.BlockSize())
+	blockMode := cipher.NewCBCEncrypter(block, key[:8])
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return crypted, nil
+}
+
+// 3DES解密
+func TripleDesDecrypt(crypted, key []byte) ([]byte, error) {
+	block, err := des.NewTripleDESCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockMode := cipher.NewCBCDecrypter(block, key[:8])
+	origData := make([]byte, len(crypted))
+	// origData := crypted
+	blockMode.CryptBlocks(origData, crypted)
+	origData = PKCS5UnPadding(origData)
+	// origData = ZeroUnPadding(origData)
+	return origData, nil
+}
+
+
+func PKCS5Padding(ciphertext []byte, blockSize int) []byte {
+	padding := blockSize - len(ciphertext)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	return append(ciphertext, padtext...)
+}
+
+func ZeroUnPadding(origData []byte) []byte {
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+func PKCS5UnPadding(origData []byte) []byte {
+	length := len(origData)
+	// 去掉最后一个字节 unpadding 次
+	unpadding := int(origData[length-1])
+	return origData[:(length - unpadding)]
+}
+
+// 加密
+func RsaEncrypt(origData []byte, publicKey []byte) ([]byte, error) {
+	block, _ := pem.Decode(publicKey)
+	if block == nil {
+		return nil, errors.New("public key error")
+	}
+	pubInterface, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	pub := pubInterface.(*rsa.PublicKey)
+	return rsa.EncryptPKCS1v15(rand.Reader, pub, origData)
+}
+
+// 解密
+func RsaDecrypt(ciphertext []byte, privateKey []byte) ([]byte, error) {
+	block, _ := pem.Decode(privateKey)
+	if block == nil {
+		return nil, errors.New("private key error!")
+	}
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return rsa.DecryptPKCS1v15(rand.Reader, priv, ciphertext)
+}
+
