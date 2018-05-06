fc69ff52da0d42b305f7f8cebc27e5f7b7f7fd30
itgeniusshuai <1115870494@qq.com>
2018-05-06 09:25:07 +0800

    大小端法字节转整形

--- common/NumberUtils.go
+++ common/NumberUtils.go
@@ -3,14 +3,12 @@ package common
 // 小端法
 func BytesToIntWithMin(bs []byte) int{
 	bLen := len(bs)
-	if(bLen < 4){
-		for m := 0; m < 4-bLen; m ++{
-			bs = append(bs, 0)
-		}
+	if(bLen > 4){
+		bLen = 4
 	}
 	var i int
 	var j uint
-	for j = 0; j < 4; j++{
+	for j = 0; j < uint(bLen); j++{
 		i |= int(bs[j])<<(8*j)
 	}
 	return i
@@ -19,14 +17,12 @@ func BytesToIntWithMin(bs []byte) int{
 // 小端法
 func BytesToInt64WithMin(bs []byte) int64{
 	bLen := len(bs)
-	if(bLen < 8){
-		for m := 0; m < 8-bLen; m ++{
-			bs = append(bs, 0)
-		}
+	if(bLen > 8){
+		bLen = 8
 	}
 	var i int64
 	var j uint
-	for j = 0; j < 8; j++{
+	for j = 0; j < uint(bLen); j++{
 		i |= int64(bs[j])<<(8*j)
 	}
 	return i
@@ -34,20 +30,28 @@ func BytesToInt64WithMin(bs []byte) int64{
 
 // 大端法
 func BytesToIntWithMax(bs []byte) int{
+	bLen := len(bs)
+	if(bLen > 4){
+		bLen = 4
+	}
 	var i int
 	var j uint
-	for j = 0; j < 4; j++{
-		i |= int(bs[3-j])<<(8*j)
+	for j = 0; j < uint(bLen); j++{
+		i |= int(bs[bLen-int(j)-1])<<(8*j)
 	}
 	return i
 }
 
 // 大端法
 func BytesToInt64WithMax(bs []byte) int64{
+	bLen := len(bs)
+	if(bLen > 8){
+		bLen = 8
+	}
 	var i int64
 	var j uint
 	for j = 0; j < 8; j++{
-		i |= int64(bs[7-j])<<(8*j)
+		i |= int64(bs[bLen-int(j)-1])<<(8*j)
 	}
 	return i
 }
