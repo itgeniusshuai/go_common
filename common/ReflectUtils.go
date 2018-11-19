7f33bc8d631b73343cb9729e2c52ea35fa5c9b61
itgeniusshuai <1115870494@qq.com>
2018-11-19 17:19:59 +0800

    方法反射执行

--- common/ReflectUtils.go
+++ common/ReflectUtils.go
@@ -1,6 +1,10 @@
 package common
 
-import "reflect"
+import (
+	"reflect"
+	"fmt"
+	"errors"
+)
 
 func ValueToInterface(v reflect.Value)interface{}{
 	var result interface{}
@@ -20,4 +24,22 @@ func ValueToInterface(v reflect.Value)interface{}{
 		case "float64":result = v.Float();break;
 	}
 	return result
+}
+
+func FuncReflectExec(execFunc interface{},params ...interface{})([]interface{},error){
+	vt := reflect.ValueOf(execFunc)
+	fmt.Println(vt.Kind())
+	paramValues := make([]reflect.Value,0)
+	if vt.Type().NumIn() != len(params){
+		return nil,errors.New("参数个数不对")
+	}
+	for _,param := range params{
+		paramValues = append(paramValues, reflect.ValueOf(param))
+	}
+	resValues := vt.Call(paramValues)
+	var res = make([]interface{},0)
+	for _,resValue := range resValues{
+		res = append(res, ValueToInterface(resValue))
+	}
+	return res,nil
 }
