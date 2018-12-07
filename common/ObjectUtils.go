434f359df88445076ce2f5d00f9d7e3cc27343e3
itgeniusshuai <1115870494@qq.com>
2018-12-07 14:49:38 +0800

    objtomap

--- common/ObjectUtils.go
+++ common/ObjectUtils.go
@@ -52,6 +52,24 @@ func ObjToMap(v interface{}) map[string]interface{}{
 	return m
 }
 
+func ObjToMapByTagName(v interface{},tagName string) map[string]interface{}{
+	var m = make(map[string]interface{},0)
+	tye := reflect.TypeOf(v)
+	value := reflect.ValueOf(v)
+	var fieldNum = tye.NumField()
+	for i := 0; i < fieldNum; i++{
+		f := tye.Field(i)
+		key := f.Name
+		tagValue := f.Tag.Get(tagName)
+		if tagValue != ""{
+			key = tagName
+		}
+		v1 := value.Field(i)
+		m[key] = ValueToInterface(v1)
+	}
+	return m
+}
+
 func MapToObj(m map[string]interface{},v interface{}) {
 	newObjValue := reflect.ValueOf(v).Elem()
 
@@ -61,6 +79,37 @@ func MapToObj(m map[string]interface{},v interface{}) {
 			f.Set(reflect.ValueOf(v))
 		}
 	}
+}
+
+func InterfacePtrToInterface(v interface{})interface{}{
+	var rv interface{}
+	switch v1 := v.(type) {
+	case *int:
+		rv = *v1
+	case *int8:
+		rv = *v1
+	case *int16:
+		rv = *v1
+	case *int32:
+		rv = *v1
+	case *int64:
+		rv = *v1
+	case *uint:
+		rv = *v1
+	case *uint8:
+		rv = *v1
+	case *uint16:
+		rv = *v1
+	case *uint32:
+		rv = *v1
+	case *float32:
+		rv = *v1
+	case *float64:
+		rv = *v1
+	case *string:
+		rv = *v1
+	}
+	return rv
 }
 
 
--- common/StructUtils.go
+++ common/StructUtils.go
@@ -4,12 +4,11 @@ import "reflect"
 import "fmt"
 
 // 获取对象对应的着重号内的标签，key为原名称，v为标签名称
-func GetStructTag(obj interface{}, tagName string) map[string]string{
-	t := reflect.TypeOf(obj)
-	n := t.NumField()
+func GetStructTag(vt reflect.Type, tagName string) map[string]string{
+	n := vt.NumField()
 	resMap := make(map[string]string)
 	for i := 0; i < n; i++{
-		st := t.Field(i)
+		st := vt.Field(i)
 		tag := st.Tag
 		tj := tag.Get(tagName)
 		resMap[st.Name] = tj
@@ -17,8 +16,7 @@ func GetStructTag(obj interface{}, tagName string) map[string]string{
 	return resMap
 }
 
-func GetFieldTagValue(obj interface{},fieldName string,tagName string) string{
-	vt := reflect.TypeOf(obj)
+func GetFieldTagValue(vt reflect.Type,fieldName string,tagName string) string{
 	f,b := vt.FieldByName(fieldName)
 	if b {
 		return f.Tag.Get(tagName)
@@ -45,3 +43,16 @@ func GetFieldValueByFieldTag(obj interface{},tagName string,tagValue string) int
 	return nil
 }
 
+func SetFieldByTagName(obj interface{}, tagName string, tagValue string, value interface{}){
+	t := reflect.TypeOf(obj)
+	tv := reflect.ValueOf(obj)
+	n := t.NumField()
+	for i := 0; i < n; i++{
+		st := t.Field(i)
+		tag := st.Tag
+		tj := tag.Get(tagName)
+		if tj == tagValue{
+			tv.Field(i).Set(reflect.ValueOf(value))
+		}
+	}
+}
